93fad0849503b46b15f783d29c60480c2b75fe5f
StonksLori <60674706+StonksLori@users.noreply.github.com>
2026-04-20 20:31:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openLetter() {
+    document.getElementById("letter").style.display = "block";
+}
+
+function closeLetter() {
+    document.getElementById("letter").style.display = "none";
+}
